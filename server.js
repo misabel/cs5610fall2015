6cd91e22a0002fc547035ec2fb58eb1fef79d4a5
Maria Irizarry <mirizarry@Marias-MacBook-Pro.local>
2015-10-19 12:56:41 -0400

    changed greeting on '/'

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', welcome);
 
 function welcome(req, res){
-	res.send("Hello!");
+	res.send("<h1> Hello! Welcome to CS5610 Fall 2015</h1>");
 }
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
