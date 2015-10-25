2515a3d17bcd288882205336382cf061ec41f4b3
Maria Irizarry <mirizarry@Marias-MacBook-Pro.local>
2015-10-25 19:25:02 -0400

    small changes to display of header options when logged in and out

--- public/assignment/header/header.controller.js
+++ public/assignment/header/header.controller.js
@@ -4,13 +4,20 @@
         .module("FormBuilderApp")
         .controller("HeaderController", HeaderController);
         
-        function HeaderController($scope, $location){
+        function HeaderController($scope, $rootScope, $location){
             
             $scope.getActive = function (path){
                 if($location.path().substr(0, path.length) === path){
                     return 'active';
                 }
                 return '';
+            }
+            $scope.logout = function(){
+                $rootScope.user = null;
+                $location.path('home');
+            }
+            $scope.loggedIn = function(){
+                return $rootScope.user;
             }
         };
 })();
